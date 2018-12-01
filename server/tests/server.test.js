687fa7d75ec1c60b61e6a194a3883df17512a8c6
August Giles <augustgiles8@gmail.com>
2018-11-30 17:11:37 -0800

    add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,22 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+let todos = [{
+  text: 'First Test Todo'
+}, {
+  text: 'Second Test Todo'
+}];
+
 // beforeEach is a lifecycle method that is obviously called before each test.
 // in this case, it's helping to configure database expectations for the tests.
+// we can make the db consistent for testing purposes
 beforeEach(done => {
   // wipes all todos
   Todo.remove({})
+    .then(() => {
+      // insertMany is a mongoose method, inserting docs into collection
+      return Todo.insertMany(todos);
+    })
     .then(() => done());
 });
 
@@ -28,7 +39,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find()
+        Todo.find({text})
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -49,7 +60,7 @@ describe('POST /todos', () => {
         }
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(e => done(e));
@@ -57,6 +68,19 @@ describe('POST /todos', () => {
   })
 })
 
+describe('GET /todos', () => {
+  it('Should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  })
+})
+
 // N O T E S   F O R   P A C K A G E . J S O N   T E S T
 // down in test and test-watch - those are custom scripts so if I run
 // the key in the command line, it will run the value. Shortcuts!!! Yay!!
+// $ npm run test-watch
